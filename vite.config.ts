fa6993ef8aef31a64e0c4c0fc86396a5d7a960e7
anaju0601 <anajulia.mm06@gmail.com>
2026-06-21 18:13:59 -0300

    ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/box-louveira-landing/",
   plugins: [react(), tailwindcss()],
 });
